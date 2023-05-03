24f761d26828f7151051ae82c5882092e02e579f
trent <dev@gmail.com>
2023-05-04 02:58:14 +0300

    add express

--- src/main.js
+++ src/main.js
@@ -6,13 +6,22 @@ import { ogg } from "./ogg.js";
 import { openai } from "./openai.js";
 import {getRandomText, responseTexts} from "./utils.js";
 import dotenv from 'dotenv'
+import express from 'express'
 
 dotenv.config()
+const app = express()
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
 const INITIAL_SESSION = {
   messages: []
 }
-console.log(process.env)
+
 const bot = new Telegraf(process.env.TELEGRAM_BOT)
 
 bot.use(session())
